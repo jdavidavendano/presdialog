ddc5dce0a5bb4c24e30719087ba789d223a90174
AlejoH97 <jaholguina@unal.edu.co>
2019-06-27 16:43:50 -0500

    disable restore pass

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['reset' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('records', 'RecordController');
