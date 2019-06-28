8b8a742d348c7e19b655305810121b986cc454c5
JDavidAvendano <jdavidavendano@gmail.com>
2019-06-27 23:22:03 -0500

    Records nav button shows depending on login status

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,10 @@
                 <a id='headerTitle' class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <a href="{{url('records')}}" class="btn">Records</a>
+                @guest
+                @else
+                    <a href="{{url('records')}}" class="btn">Records</a>
+                @endguest
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
