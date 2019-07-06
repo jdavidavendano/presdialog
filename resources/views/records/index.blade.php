d1442815564ab492adf3d52292463d954bba94df
JDavidAvendano <jdavidavendano@gmail.com>
2019-07-05 21:13:13 -0500

    removing a button :)

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -22,9 +22,6 @@
   <h2> Records </h2>
   <br>
   <br>
-  <a href="{{ route('records.create')}}" class="btn btn-primary">Create</a>
-  <br>
-  <br>
   <table class="table table-striped">
     <thead>
         <tr>
